df45489be3cad6928a6c7a92b7114d9148af3f21
Ashish Prasad <aspras@microsoft.com>
2022-04-01 16:52:15 +0530

    updaed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
+
 function App() {
+  fetch('/api/createinvite')
+      .then(response => response.json());
+
   const value = 'World';
   return <div>Hello {value}</div>;
 }
